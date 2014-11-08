c465e2682c9bf4d3cb715f44a6dc2727ad9a364d
Pat Schloss <pschloss@umich.edu>
2014-11-08 14:36:07 -0500

    Remove several options from script

--- compile_docx.sh
+++ compile_docx.sh
@@ -1,4 +1,4 @@
 MDFILE=$1
 DOCXFILE=${MDFILE//.md/.docx}
 
-pandoc $MDFILE --to docx --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash-implicit_figures --output $DOCXFILE --highlight-style tango --reference-docx template.docx
+pandoc $MDFILE --to docx --from markdown+tex_math_single_backslash-implicit_figures --output $DOCXFILE --highlight-style tango --reference-docx template.docx
